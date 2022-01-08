d42fc43559a894ec70f4b1a867ec39af12cf9e76
sanghyunkim <shk204105@gmail.com>
2022-01-08 17:07:42 +1100

    Code edit
    Edited feature selection processes
    Added linear regression assumption checks

--- Code.py
+++ Code.py
@@ -8,11 +8,12 @@ import pingouin as pg
 from sklearn import linear_model, metrics
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.preprocessing import StandardScaler
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-from sklearn.feature_selection import SelectKBest, f_regression, RFE
+from sklearn.feature_selection import RFE,  SelectKBest, f_regression
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
 import statsmodels.api as sm
 from scipy import stats
+from yellowbrick.regressor import residuals_plot
 from math import sqrt
 import warnings
 warnings.filterwarnings('ignore')
@@ -45,6 +46,9 @@ print(batting_df.isnull().sum())
 
 # check duplicated data
 print("Total number of duplicates in batting data: {}".format(batting_df.duplicated().sum()))
+
+# create a new column for Total Base data "TB"
+batting_df["TB"] = batting_df["1B"] + (2 * batting_df["2B"]) + (3 * batting_df["3B"]) + (4 * batting_df["HR"])
 
 # create league data: National League (NL) / American League (AL)
 nl_teams = ["ATL", "CHC", "CIN", "COL", "LAD",
@@ -86,9 +90,10 @@ cols = ["Season", "League"] + list(batting_df.columns)[1:-1]
 batting_df = batting_df.reindex(columns = cols)
 
 
+
 # 2. EDA (Exploratory Data Analysis)
 # 2-1. RS Analysis: How did the league average runs scored change over time?
-print("------- Runs Scored Data Descriptive Summary (Seasonal) -------")
+print("------- Runs Scored Data Descriptive Summary by Season -------")
 print(batting_df["RS"].describe())
 
 season_df = batting_df.groupby("Season")
@@ -122,10 +127,10 @@ ax.set_xticklabels(ax.get_xticklabels(), rotation = 45)
 ax.set(title = "Yearly Changes in Team Runs Scored")
 plt.show()
 
-# 2-2. RS Analysis: In which League (NL vs AL) did teams scored runs more?: Two-sample t-test
+# 2-2. RS Analysis: In which League (NL vs AL) did teams scored more?: Two-sample t-test
 # group data based on "League" data values
 lg_df = batting_df.groupby("League")
-print("------- Runs Scored Data Descriptive Summary (League) -------")
+print("------- Runs Scored Data Descriptive Summary by League -------")
 print(lg_df["RS"].describe())
 
 nl_rs = batting_df.loc[batting_df["League"] == "NL"]["RS"]
@@ -166,265 +171,246 @@ plt.show()
 
 # Welch's two-sample t-test
 test_result = pg.ttest(al_rs, nl_rs, paired = False, alternative='greater', correction = True).round(3)
+print("Welch's two-sample t-test result")
 print(test_result.to_string())
+# given the p-value is approximately 0, we reject H0
+# and have a strong evidence that AL teams scored more than NL teams on average
 
-# # 'RS' histogram and Q-Q plot
-# fig, axes = plt.subplots(1, 2, figsize=(20, 8))
-#
-# sns.histplot(batting_df['RS'], kde = True, ax = axes[0])
-# axes[0].set_title('Team RS Histogram')
-#
-# axes[1] = stats.probplot(batting_df['RS'], plot=plt)
-# plt.title('Team RS Q-Q Plot')
-#
-# plt.show()
-#
-# print('------- Team RS Data Distribution -------')
-# print('Mean RS: {}'.format(batting_df['RS'].mean()))
-# print('Median RS: {}'.format(batting_df['RS'].median()))
-# print('RS Standard Deviation: {}'.format(batting_df['RS'].std()))
-# print('RS Skewness: {}'.format(batting_df['RS'].skew()))
-# print('RS Kurtosis: {}'.format(batting_df['RS'].kurt()))
-#
-# # yearly changes in RS
-# yearly_rs = pd.concat([batting_df['YEAR'], batting_df['RS']], axis=1)
-# fig, ax = plt.subplots(figsize=(10, 10))
-#
-# sns.boxplot(x='YEAR', y='RS', data=yearly_rs, ax=ax)
-# ax.set(title='Yearly Changes in Team Runs Scored')
-#
-# plt.show()
-#
-# # correlation matrix
-# corrMatrix = batting_df.corr()
-# fig, ax = plt.subplots(figsize=(8, 8))
-#
-# sns.heatmap(corrMatrix, square=True)
-# plt.title('Correlation Matrix')
-#
-# plt.show()
-#
-# # multicollinearity detection
-# # 1. drop independent variables if its correlation between other independent variables is higher than 0.95
-# no_target = batting_df.iloc[:, batting_df.columns != 'RS']
-#
-# corrMatrix = abs(no_target.corr())
-# upperTri = corrMatrix.where(np.triu(np.ones(corrMatrix.shape), k=1).astype(np.bool))
-# vars_drop = [col for col in upperTri.columns if any(upperTri[col] > 0.95)]
-#
-# df = batting_df.drop(vars_drop, axis=1)
-#
-# # 2. drop variables that have lower correlations than 0.60 with 'RS'
-# corrMatrix = abs(df.corr())
-# cols = list(corrMatrix.columns)
-#
-# for col in cols:
-#     if corrMatrix[col]['RS'] < 0.65:
-#         vars_drop = col
-#         df.drop(col, axis=1, inplace=True)
-#
-# filtered_vars = list(df.columns)
-# print('Filtered Variables: {}'.format(filtered_vars))
-#
-# df = df[filtered_vars]
-#
-# # new correlation matrix for filtered data features
-# fig, ax = plt.subplots(figsize=(10, 10))
-#
-# corrMatrix = df.corr()
-# sns.heatmap(corrMatrix, square=True, annot=True, annot_kws={'size':10},
-#             xticklabels=corrMatrix.columns, yticklabels=corrMatrix.columns)
-# plt.title('Correlation Matrix')
-#
-# plt.show()
-#
-# # independent variables EDA
-# cols = list(df.drop('RS', axis=1).columns)
-# # histograms
-# fig, axes = plt.subplots(3, 3, figsize=(15, 15))
-#
-# for col, ax in zip(cols, axes.flatten()[:8]):
-#     sns.histplot(df[col], kde=True, color='red', ax=ax)
-#     ax.set_title('Team {} Histogram'.format(col))
-#
-# plt.show()
-#
-# # Q-Q plots
-# fig, axes = plt.subplots(3, 3, figsize=(17, 17))
-#
-# for col, ax in zip(cols, axes.flatten()[:8]):
-#     stats.probplot(df[col], plot=ax)
-#     ax.set_title('{} Q-Q Plot'.format(col))
-#
-# plt.show()
-#
-# # scatter plots
-# fig, axes = plt.subplots(3, 3, figsize=(15, 15))
-#
-# for col, ax in zip(cols, axes.flatten()[:8]):
-#     sns.regplot(x=col, y='RS', data=df, scatter_kws={'color': 'navy'},
-#                 line_kws={'color': 'red'}, ax=ax)
-#     ax.set_title('Correlation between {} and RS'.format(col))
-#     ax.set_xlabel(col)
-#     ax.set_ylabel('RS')
-#
-# plt.show()
-#
-#
-#
-# ### 3. Feature Scaling ###
-# # check independent data ranges
-# print(df.describe().to_string())
-#
-# # StandardScaler
-# scaled_df = df.drop(['RS'], axis=1)
-# cols = list(scaled_df.columns)
-#
-# std_scaler = StandardScaler()
-# scaled_data = std_scaler.fit_transform(scaled_df)
-# scaled_df = pd.DataFrame(scaled_data, columns=cols)
-#
-# # KDE plot after scaling
-# scaled_cols = list(scaled_df.columns)
-# fig, ax = plt.subplots(figsize=(10, 10))
-#
-# for col in scaled_cols:
-#     sns.kdeplot(scaled_df[col], label=col, ax=ax)
-#     ax.set_title('After StandardScaler')
-#     ax.set_xlabel('Data Scale')
-#     ax.legend(loc=1)
-#
-# plt.show()
-#
-#
-#
-# ### 4. Multiple Linear Regression with feature selection ###
-# # Recursive Feature Elimination
-# df = pd.concat([df['RS'], scaled_df], axis=1)
-#
-# x = df.loc[:, df.columns != 'RS']
-# y = df['RS']
-# cols = list(x.columns)
-#
-# lm = LinearRegression()
-#
-# rfe = RFE(lm, 2)
-# x_rfe = rfe.fit_transform(x, y)
-# lm.fit(x_rfe, y)
-#
-# temp = pd.Series(rfe.support_, index=cols)
-# selected_vars = list(temp[temp == True].index)
-# print('Selected Features: {}'.format(selected_vars))
-#
-# # check VIF
-# x = df[selected_vars]
-# x = sm.add_constant(x)
-# y = df['RS']
-#
-# lm = sm.OLS(y, x)
-# result_rs = lm.fit()
-# print(result_rs.summary())
-#
-# vif = pd.DataFrame()
-# vif['Feature'] = lm.exog_names
-# vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
-# print(vif[vif['Feature'] != 'const'])
-#
-#
-# # split data into training and test data and build a multiple linear regression model
-# # multiple linear regression (x:'TB', 'OBP' / y:'RS')
-# x = df[selected_vars]
-# y = df['RS']
-#
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
-#
-# lm = linear_model.LinearRegression().fit(x_train, y_train)
-#
-# y_predict = lm.predict(x_test)
-#
-# print('------- Multiple Linear Regression -------')
-# print('------- Intercept -------')
-# print(lm.intercept_)
-#
-# print('------- Coefficient -------')
-# print(lm.coef_)
-#
-# print('------- RMSE -------')
-# mse = metrics.mean_squared_error(y_test, y_predict)
-# print(sqrt(mse))
-#
-# print('------- R-squared -------')
-# print(metrics.r2_score(y_test, y_predict))
-#
-#
-#
-# ### 5. Simple Linear Regression ###
-# # univariate feature selection
-# x = batting_df.loc[:, batting_df.columns != 'RS']
-# y = batting_df['RS']
-#
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
-#
-# selector = SelectKBest(score_func=f_regression, k=1)
-# selected_xTrain = selector.fit_transform(x_train, y_train)
-# selected_xTest = selector.transform(x_test)
-#
-# all_cols = x.columns
-# selected_mask = selector.get_support()
-# selected_var = all_cols[selected_mask].values
-#
-# print('Selected Feature: {}'.format(selected_var))
-#
-# # simple linear regression (x:'OPS' / y:'RS')
-# x = np.array(batting_df[selected_var]).reshape(-1, 1)
-# y = batting_df['RS']
-#
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
-#
-# lm = linear_model.LinearRegression().fit(x_train, y_train)
-#
-# y_predicted = lm.predict(x_test)
-#
-# print('------- Simple Linear Regression -------')
-# print('------- Intercept -------')
-# print(lm.intercept_)
-#
-# print('------- Coefficient -------')
-# print(lm.coef_)
-#
-# print('------- RMSE -------')
-# mse = metrics.mean_squared_error(y_test, y_predicted)
-# print(sqrt(mse))
-#
-# print('------- R-squared -------')
-# print(metrics.r2_score(y_test, y_predicted))
-#
-#
-#
-# ### 6. Model Validation ###
-#
-# # 10-Fold Cross Validation for the multiple linear regression model
-# model = LinearRegression()
-# x = df[['TB', 'OBP']]
-# y = df['RS']
-#
-# cv_r2 = cross_val_score(model, x, y, scoring='r2', cv=10)
-# cv_mse = cross_val_score(model, x, y, scoring='neg_mean_squared_error', cv=10)
-# cv_rmse = np.sqrt(-1 * cv_mse)
-#
-# print('------- Multiple Linear Regression Validation -------')
-# print('Mean R-squared: {}'.format(cv_r2.mean()))
-# print('Mean RMSE: {}'.format(cv_rmse.mean()))
-#
-# # 10-Fold Cross Validation for the simple linear regression model
-# model = LinearRegression()
-# x = np.array(batting_df['OPS']).reshape(-1, 1)
-# y = batting_df['RS']
-#
-# cv_r2 = cross_val_score(model, x, y, scoring='r2', cv=10)
-# cv_mse = cross_val_score(model, x, y, scoring='neg_mean_squared_error', cv=10)
-# cv_rmse = np.sqrt(-1 * cv_mse)
-#
-# print('------- Simple Linear Regression Validation -------')
-# print('Mean R-squared: {}'.format(cv_r2.mean()))
-# print('Mean RMSE: {}'.format(cv_rmse.mean()))
+# 'RS' histogram and Q-Q plot
+fig, axes = plt.subplots(1, 2, figsize=(20, 8))
+
+sns.histplot(batting_df['RS'], kde = True, ax = axes[0])
+axes[0].set_title('Team RS Histogram')
+
+axes[1] = stats.probplot(batting_df['RS'], plot=plt)
+plt.title('Team RS Q-Q Plot')
+
+plt.show()
+
+print('------- Team RS Data Distribution -------')
+print('Mean RS: {}'.format(batting_df['RS'].mean()))
+print('Median RS: {}'.format(batting_df['RS'].median()))
+print('RS Skewness: {}'.format(batting_df['RS'].skew()))
+print('RS Kurtosis: {}'.format(batting_df['RS'].kurt()))
+
+
+
+# 3. Feature Selection
+# correlation matrix
+corrMatrix = batting_df.corr()
+fig, ax = plt.subplots(figsize = (8, 8))
+
+sns.heatmap(corrMatrix, square = True)
+plt.title('Correlation Matrix')
+plt.show()
+
+# 3-1. first drop variables that have lower (absolute) correlations with 'RS' than 0.65
+corrMatrix = abs(batting_df.corr())
+cols = list(corrMatrix.columns)
+vars_to_drop = []
+for col in cols:
+    if corrMatrix[col]['RS'] < 0.65:
+        vars_to_drop.append(col)
+
+filtered_df = batting_df.loc[:, ~batting_df.columns.isin(vars_to_drop)]
+filtered_vars = list(filtered_df.columns)
+
+# 3-2. drop any independent variables if its pairwise correlation between other independent variables is higher than 0.9
+ind_vars_df = filtered_df.iloc[:, filtered_df.columns != 'RS']
+
+corrMatrix = abs(filtered_df.corr())
+upperTri = corrMatrix.where(np.triu(np.ones(corrMatrix.shape), k = 1).astype(np.bool))
+vars_to_drop = [col for col in upperTri.columns if any(upperTri[col] >= 0.9)]
+filtered_df.drop(vars_to_drop, axis = 1, inplace = True)
+
+# new correlation matrix for filtered data features
+fig, ax = plt.subplots(figsize = (10, 10))
+
+corrMatrix = filtered_df.corr()
+sns.heatmap(corrMatrix, square = True, annot = True, annot_kws = {'size': 10},
+            xticklabels = corrMatrix.columns, yticklabels = corrMatrix.columns)
+plt.title('Correlation Matrix')
+
+plt.show()
+
+# 3-3. Recursive Feature Elimination
+# select numerical data
+num_df = filtered_df.select_dtypes(exclude = "category")
+x = num_df.loc[:, num_df.columns != 'RS']
+y = num_df['RS']
+cols = list(x.columns)
+
+lm = LinearRegression()
+
+rfe = RFE(lm, 2)
+x_rfe = rfe.fit_transform(x, y)
+lm.fit(x_rfe, y)
+
+temp = pd.Series(rfe.support_, index = cols)
+selected_vars = list(temp[temp == True].index)
+print('RFE Features: {}'.format(selected_vars))
+
+# check VIF
+x = num_df[selected_vars]
+x = sm.add_constant(x)
+y = num_df['RS']
+
+lm = sm.OLS(y, x)
+result_rs = lm.fit()
+print(result_rs.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'])
+
+# 3-4. backward selection
+lm = LinearRegression()
+num_df = batting_df.select_dtypes(exclude = "category")
+x = num_df.loc[:, num_df.columns != 'RS']
+y = batting_df['RS']
+sfs = SFS(lm, k_features = 2, forward = False, verbose = 2,
+          scoring = "r2", cv = 0, n_jobs = -1)
+sfs.fit(x, y)
+print("\nBackward Selection Features: {}\n".format(sfs.k_feature_names_))
+# from both Recursive Feature Elimination and Backward Selection methods,
+# I ended up with two most significant independent variables: "OBP" and "ISO"
+
+# select final features
+filtered_df = batting_df.loc[:, ["RS", "OBP", "ISO"]]
+
+
+
+# 4. Multiple Linear Regression with feature selection
+
+# split data into training and test data and build a multiple linear regression model
+# multiple linear regression (x:'OBP', 'ISO' / y:'RS')
+x = filtered_df[selected_vars]
+y = filtered_df['RS']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+mlr = linear_model.LinearRegression().fit(x_train, y_train)
+
+y_predict = mlr.predict(x_test)
+resid = y_test - y_predict
+
+# 4-1. Assumption checking
+# linearity
+# scatter plots
+cols = list(filtered_df.drop('RS', axis = 1).columns)
+
+fig, axes = plt.subplots(1, 2, figsize = (18, 8))
+
+for col, ax in zip(cols, axes.flatten()[:2]):
+    sns.regplot(x = col, y = 'RS', data = filtered_df, scatter_kws = {'color': 'navy'},
+                line_kws = {'color': 'red'}, ax = ax)
+    ax.set_title('Correlation between {} and RS'.format(col))
+    ax.set_xlabel(col)
+    ax.set_ylabel('RS')
+
+plt.show()
+
+# homoscedasticity
+# residual plot
+residuals_plot(LinearRegression(), x_train, y_train, x_test, y_test)
+
+# normality
+# QQ plot
+fig = plt.subplots(figsize = (8, 8))
+stats.probplot(resid, dist = "norm", plot = plt)
+plt.title("Multiple Linear Regression QQ Plot")
+plt.show()
+
+# independence
+# given that data observations are independent of each other,
+# the independence assumption is satisified
+
+# multiple linear regression results
+print('------- Multiple Linear Regression -------')
+print('------- Intercept -------')
+print(mlr.intercept_)
+
+print('------- Coefficient -------')
+print(mlr.coef_)
+
+print('------- RMSE -------')
+mse = metrics.mean_squared_error(y_test, y_predict)
+print(sqrt(mse))
+
+print('------- R-squared -------')
+print(metrics.r2_score(y_test, y_predict))
+
+
+
+# 5. Simple Linear Regression
+# univariate feature selection
+num_df = batting_df.select_dtypes(exclude = "category")
+x = num_df.loc[:, num_df.columns != 'RS']
+y = batting_df['RS']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+selector = SelectKBest(score_func = f_regression, k = 1)
+selected_xTrain = selector.fit_transform(x_train, y_train)
+selected_xTest = selector.transform(x_test)
+
+all_cols = x.columns
+selected_mask = selector.get_support()
+selected_var = all_cols[selected_mask].values
+
+print('Simple Linear Regression Selected Feature: {}'.format(selected_var))
+
+# simple linear regression (x:'OPS' / y:'RS')
+x = np.array(batting_df[selected_var]).reshape(-1, 1)
+y = batting_df['RS']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+lm = linear_model.LinearRegression().fit(x_train, y_train)
+
+y_predicted = lm.predict(x_test)
+
+print('------- Simple Linear Regression -------')
+print('------- Intercept -------')
+print(lm.intercept_)
+
+print('------- Coefficient -------')
+print(lm.coef_)
+
+print('------- RMSE -------')
+mse = metrics.mean_squared_error(y_test, y_predicted)
+print(sqrt(mse))
+
+print('------- R-squared -------')
+print(metrics.r2_score(y_test, y_predicted))
+
+
+
+# 6. Model Validation
+
+# 10-Fold Cross Validation for the multiple linear regression model
+model = LinearRegression()
+x = filtered_df[['OBP', 'ISO']]
+y = filtered_df['RS']
+
+cv_r2 = cross_val_score(model, x, y, scoring = 'r2', cv = 10)
+cv_mse = cross_val_score(model, x, y, scoring = 'neg_mean_squared_error', cv = 10)
+cv_rmse = np.sqrt(-1 * cv_mse)
+
+print('------- Multiple Linear Regression Validation -------')
+print('Mean R-squared: {}'.format(cv_r2.mean()))
+print('Mean RMSE: {}'.format(cv_rmse.mean()))
+
+# 10-Fold Cross Validation for the simple linear regression model
+model = LinearRegression()
+x = np.array(batting_df['OPS']).reshape(-1, 1)
+y = batting_df['RS']
+
+cv_r2 = cross_val_score(model, x, y, scoring='r2', cv=10)
+cv_mse = cross_val_score(model, x, y, scoring='neg_mean_squared_error', cv=10)
+cv_rmse = np.sqrt(-1 * cv_mse)
+
+print('------- Simple Linear Regression Validation -------')
+print('Mean R-squared: {}'.format(cv_r2.mean()))
+print('Mean RMSE: {}'.format(cv_rmse.mean()))
